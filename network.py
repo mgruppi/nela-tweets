f8fa7ca84f7539ff267d4dc496d3b10fd90202cf
mgruppi <mauricio.gruppi@gmail.com>
2021-03-18 19:38:19 -0400

    Add tweets_per_source.py.

--- network.py
+++ network.py
@@ -2,18 +2,60 @@ import networkx as nx
 import argparse
 import sqlite3
 import re
+import numpy as np
+import json
 
 
-def load_all_tweets(con):
+def load_user_data(path):
+    """
+    Load JSON file containing Twitter user data.
+    :param path: Path to JSON file.
+    :return user_data: JSON object indexed by username.
+    """
+    user_data = dict()
+    with open(path) as fin:
+        data = json.load(fin)
+
+    for user in data:
+        user_data[user["username"]] = user
+    return user_data
+
+
+def load_all_tweets(con, row_ids=None):
     """
     Load all data in table `tweet`. Results are ordered by `embedded_tweet` which corresponds to the tweets' URL.
     :param con: Sqlite3 connection to NELA database.
+    :param row_ids: Article rowids to include. Only return tweets that appear in articles in row_ids.
     :return result: List of results, one tweet per row.
     """
-    query = "SELECT t.article_id as article_id, a.source as source, embedded_tweet as url " \
+
+    query = "SELECT t.article_id as article_id, a.source as source, embedded_tweet as url, a.rowid as rowid " \
             "FROM tweet t INNER JOIN newsdata a ON t.article_id = a.id"
-    result = con.cursor().execute(query).fetchall()
+    if row_ids is None:
+        result = con.cursor().execute(query).fetchall()
+    else:
+        result = list()
+        rid_set = set(row_ids)
+        all_results = con.cursor().execute(query).fetchall()
+        for i, r in enumerate(all_results):
+            print(r)
+            if r[-1] in rid_set:
+                result.append(r)
     return result
+
+
+def load_article_rowids(path):
+    """
+    Load and returns article row ids for a given topic.
+    :param path: Path to file.
+    :return row_ids: List of rowids, this field can be used to query a sqlite3 database.
+    """
+    with open(path) as fin:
+        fin.readline()  # read header
+        data = map(lambda s: s.strip().split(",", 2), fin.readlines())
+        row_ids, months, sources = zip(*data)
+
+    return row_ids
 
 
 def clean_tweet_id(_id):
@@ -108,17 +150,19 @@ def build_network(tweets, cutoff=1):
     return g
 
 
-def build_source_network(tweets, cutoff=1):
+def build_source_network(tweets, user_data):
     """
     Builds a network of source-source relationships.
     Two nodes u, v represent sources that are connected if they share a common embedded tweet author.
-    :param tweets:
-    :param cutoff: Cutoff value for edge weights. Only weights > `cutoff` will be included as edges.
+    :param tweets: Tweet data.
+    :param user_data: Dictionary keyed by username, containing user information.
     :return: g NetworkX undirected graph.
     """
 
     # We will begin by constructing a dictionary mapping author -> source -> weight
-    authors = dict()
+    # weight is the number of times a source cites an author
+    authors = dict()  # dict that stores author -> source -> no. of citations
+    sources = dict()  # dict that stores source -> author -> no. of citations
 
     for t in tweets:
         _id = clean_tweet_id(t[2])
@@ -128,23 +172,76 @@ def build_source_network(tweets, cutoff=1):
             authors[t_author] = dict()
         if src not in authors[t_author]:
             authors[t_author][src] = 0
+        if src not in sources:
+            sources[src] = dict()
+        if t_author not in sources[src]:
+            sources[src][t_author] = 0
 
         authors[t_author][src] += 1
-    ebunch = list()
+        sources[src][t_author] += 1
+
+    # We will compute the main (u, v) edge weight as the probability that, given a set of common references S,
+    # and given that u and v will cite, the u and v cite a common source in S.
+    # We will treat the citations by u and v of a reference A as independent events.
+
+    for src in sources:
+        num_references = sum(sources[src].values())
+        for a in sources[src]:  # compute probability that src cites a.
+            sources[src][a] = sources[src][a]/num_references
 
     g = nx.Graph()
-    for a in authors:
-        for i, src_a in enumerate(authors[a]):
-            for j, src_b in enumerate(list(authors[a].keys())[i+1:]):  # create pairwise links
-                g.add_edge(src_a, src_b, weight_a=authors[a][src_a], weight_b=authors[a][src_b])
+    p_threshold = 0.01
+    x = list()  # store edge weight distribution.
+    ebunch = list()  # store edge (u, v, weight) tuples.
+    # Iterate over each pair of sources in `sources`.
+    num_pairs = len(sources)**2
+    for i, u in enumerate(sources.keys()):
+        for j, v in enumerate(list(sources.keys())[i+1:]):
+            print("Computing edge weights (%2.2f)" % (100*(i*len(sources)+j+1)/num_pairs), end="\r")
+            common_refs = set.intersection(set(sources[u]), set(sources[v]))
+            prob = 0
+            for author in common_refs:
+                if author in user_data:
+                    scaling_factor = 1/(np.log(user_data[author]["public_metrics"]["followers_count"]))
+                else:
+                    scaling_factor = 1
+                prob += sources[u][author]*sources[v][author] * scaling_factor
+
+            ebunch.append((u, v, prob))
+            x.append(prob)
+    print()
+    x = np.array(x, dtype=np.float32)
+    print("Mean edge weight(prob):", x.mean(), "+-", x.std())
+
+    e_bunch_filter = list()
+
+    for e in ebunch:  # (u, v, weight) tuples
+        if e[2] > p_threshold:
+            e_bunch_filter.append(e)
+    g.add_weighted_edges_from(e_bunch_filter)
+    #  -- deprecated --
+    # for a in authors:
+    #     for i, src_a in enumerate(authors[a]):
+    #         for j, src_b in enumerate(list(authors[a].keys())[i+1:]):  # create pairwise links
+    #             g.add_edge(src_a, src_b, weight_a=authors[a][src_a], weight_b=authors[a][src_b])
     return g
 
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument("output", type=str, help="Path to output network.")
     args = parser.parse_args()
 
-    path_gml = "graph.gml"
+    path_user_data = "user_data/user_data.json"
+    path_gml = args.output
+    path_row_ids = "../data/nela/extracted_articles/all_covid_articles.txt"
+
+    row_ids = load_article_rowids(path_row_ids)
+    print("-- Topic articles:", len(row_ids))
+
+    user_data = load_user_data(path_user_data)
+
+    print("-- User data", len(user_data))
 
     path_labels = "data/labels.csv"
     with open(path_labels) as fin:
@@ -154,17 +251,17 @@ def main():
     path = "../data/dataverse/release/nela-gt-2020.db"
     con = sqlite3.connect(path)
 
-    tweets = load_all_tweets(con)
-    article_ids = get_unique_articles(con)
-
+    tweets = load_all_tweets(con, row_ids=row_ids)
     t_ids = [t[2] for t in tweets]
     t_authors = get_tweet_authors(t_ids)
 
+    found = sum(author in user_data for author in t_authors)
+
+    print("Authors", found, len(t_authors))
     print("Loaded %d tweets from %d authors." % (len(tweets), len(t_authors)))
-    print("Found %d article ids." % len(article_ids))
 
     # g = build_network(tweets, cutoff=5)
-    g = build_source_network(tweets, cutoff=5)
+    g = build_source_network(tweets, user_data)
 
     print("Setting node attributes...")
     for n in g.nodes:
@@ -177,17 +274,7 @@ def main():
                 g.nodes[n]["cred"] = -1.0
             else:
                 g.nodes[n]["cred"] = 0.0
-        # elif n in t_authors:
-        #     g.nodes[n]["credibility"] = "N/A"
-        #     g.nodes[n]["class"] = "twitter"
-        #     g.nodes[n]["cred"] = 0.0
-        #     for edge in g.edges(n):
-        #         if edge[1] in labels:
-        #             if labels[edge[1]] == "0":
-        #                 g.nodes[n]["cred"] += 1.
-        #             elif labels[edge[1]] == "1" or labels[edge[1]] == "2":
-        #                 g.nodes[n]["cred"] += -1.
-        #     g.nodes[n]["cred"] = g.nodes[n]["cred"]/len(g.edges(n))
+
         else:
             g.nodes[n]["credibility"] = "unlabeled"
             g.nodes[n]["class"] = "news"
---
+++ tweets_per_source.py
@@ -0,0 +1,31 @@
+import sqlite3
+import numpy as np
+
+
+files = ["/data/NELA-GT-2018/nela-gt-2018.db", "/data/NELA-GT-2019/nela-gt-2019.db",
+         "/data/NELA-GT-2020/nela-gt-2020.db"]
+
+query = " SELECT d.source, count(t.id) FROM " \
+        " tweet t INNER JOIN newsdata d " \
+        " ON t.article_id = d.id " \
+        " GROUP BY source"
+
+print(query)
+files = ["../data/dataverse/release/nela-gt-2020.db", "../data/dataverse/release/nela-gt-2020.db", "../data/dataverse/release/nela-gt-2020.db"]
+
+tweet_count = dict()
+for i, path in enumerate(files):
+        con = sqlite3.connect(path)
+        result = con.execute(query)
+        for row in result.fetchall():
+                if row[0] not in tweet_count:
+                        tweet_count[row[0]] = np.zeros(len(files), dtype=np.int32)
+                tweet_count[row[0]][i] = row[1]
+
+for src in sorted(tweet_count):
+        print(src, tweet_count[src])
+path_out = "tweets-per-source.csv"
+with open(path_out, "w") as fout:
+        fout.write("source,2018,2019,2020\n")
+        for src in sorted(tweet_count):
+                fout.write("%s,%d,%d,%d\n" % (src, tweet_count[src][0], tweet_count[src][1], tweet_count[src][2]))
